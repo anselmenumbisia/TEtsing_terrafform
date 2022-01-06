87904dfda05fe721d30661c78e26ddf5896c92ef
anselmenumbisia <70728885+anselmenumbisia@users.noreply.github.com>
2022-01-06 15:37:24 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
